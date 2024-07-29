b7361bf66baf6e13ed5d9c5449845d683a4b17ed
atuldemond <atuldemond@gmail.com>
2024-07-29 14:30:34 +0530

    We are working on backend

--- 9.Quiz App/backend/app.js
+++ 9.Quiz App/backend/app.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("You are on  Home page Bro  ");
 });
 
+app.get("*", (req, res) => {
+  res.send("Page Not found you are not entering worng url ");
+});
+
 app.listen(port, (req, res) => {
   console.log("sever is running on 3000 port");
 });
