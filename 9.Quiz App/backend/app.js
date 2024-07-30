cecf28400c3700d3d85adda72f41bbd795fc0000
atuldemond <atuldemond@gmail.com>
2024-07-30 14:39:07 +0530

    We are working on backend

--- 9.Quiz App/backend/app.js
+++ 9.Quiz App/backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const path = require("path");
 
 app.get("/", (req, res) => {
   res.send("You are on  Home page Bro  ");
